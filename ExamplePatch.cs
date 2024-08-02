8b73d33d58076520c018ea55cb205fb3dde93eae
none <none@example.com>
2024-08-02 13:10:07 -0400

    init

---
+++ ExamplePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace QM_Template
+{
+    [HarmonyPatch(typeof(MainMenuScreen), nameof(MainMenuScreen.Awake))]
+    public static class ExamplePatch
+    {
+        public static void Prefix(MainMenuScreen __instance)
+        {
+            Debug.Log("--- main menu awake");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace QM_Template
+{
+    public static class Plugin
+    {
+        public static string ModAssemblyName => Assembly.GetExecutingAssembly().GetName().Name;
+
+        public static string ConfigPath => Path.Combine(Application.persistentDataPath, ModAssemblyName, "config.json");
+        public static string ModPersistenceFolder => Path.Combine(Application.persistentDataPath, ModAssemblyName);
+        public static ModConfig Config { get; private set; }
+
+        [Hook(ModHookType.AfterConfigsLoaded)]
+        public static void AfterConfig(IModContext context)
+        {
+            Directory.CreateDirectory(ModPersistenceFolder);
+
+            Config = ModConfig.LoadConfig(ConfigPath);
+
+            new Harmony("_" + ModAssemblyName).PatchAll();
+        }
+
+     
+    }
+}
